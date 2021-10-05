4cd048619731ead6f56a83ee3dcdbf18758aa518
Salsa Gal <fielddashos@gmail.com>
2021-10-05 18:40:35 +1100

    Add boot string function

--- src/boot.asm
+++ src/boot.asm
@@ -4,7 +4,27 @@ boot_start:
     mov ax, 0x07c0
     mov ds, ax
 
+    mov si, boot_text_start
+    call boot_string
+
     jmp $
+
+boot_string:
+    push ax
+    mov ah, 0xe
+
+.loop:
+    lodsb
+    cmp al, 0
+    je .end
+    int 0x10
+    jmp .loop
+
+.end:
+    pop ax
+    ret
+
+boot_text_start: db "Boot start",0
 
 times 510-($-$$) db 0
 dw 0xaa55
