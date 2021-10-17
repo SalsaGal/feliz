51835e68bde61f21ed03c86bdc655ce3446dd593
Salsa Gal <fielddashos@gmail.com>
2021-10-17 15:31:50 +1100

    Clear the buffer

--- src/kernel.asm
+++ src/kernel.asm
@@ -16,6 +16,12 @@ feliz_kernel_start:
     mov si, shell_buffer
     call feliz_shell_print_line
 
+    mov al, 0
+.clear_buffer:
+    stosb
+    cmp di, shell_buffer_end + 1
+    jle .clear_buffer
+
     jmp .shell
 
 feliz_kernel_text_start: db "Kernel: Started", 0
@@ -23,4 +29,5 @@ feliz_kernel_text_start: db "Kernel: Started", 0
 %include "keyboard.asm"
 %include "shell.asm"
 
-shell_buffer: times 32 db 0
+shell_buffer: times 80 db 0
+shell_buffer_end:
